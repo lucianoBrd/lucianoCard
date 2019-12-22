bf6a87c1ece57b2a37aad0d8dc3fa16098eb6b32
lucianoBrd <lucien.burdet@gmail.com>
2019-12-22 12:25:52 +0100

    add circle avatar

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,16 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: <Widget>[
+              CircleAvatar(
+                radius: 100.0,
+                backgroundImage: AssetImage('images/luciano_work.jpg'),
+              ),
+              Text(
+                'Lucien Burdet',
+                style: TextStyle(
 
+                ),
+              ),
             ],
           ),
         ),
